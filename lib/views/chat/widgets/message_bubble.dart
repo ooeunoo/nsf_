a8cd20bb8a956e7a1b1df8c2203d495f28e274b7
seongeun <seongeun.cho.dev@gmail.com>
2024-04-12 17:02:10 +0900

    bubble

--- lib/views/chat/widgets/message_bubble.dart
+++ lib/views/chat/widgets/message_bubble.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nsf/models/message/message_model.dart';
-import 'package:nsf/models/user/user_model.dart';
 import 'package:nsf/utils/styles/color.dart';
 import 'package:nsf/utils/styles/dimens.dart';
 import 'package:nsf/utils/styles/font.dart';
@@ -24,128 +23,163 @@ class MessageBubble extends StatelessWidget {
         top: AppDimens.size20,
       ),
       child: Row(
-          mainAxisAlignment:
-              message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: message.isMine
-                  ? MediaQuery.of(context).size.width * 0.7
-                  : MediaQuery.of(context).size.width * 0.8,
-              child:
-                  message.isMine ? _myBubble(context) : _otherBubble(context),
-            ),
-          ]),
+        mainAxisAlignment:
+            message.isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          message.isMine ? _myBubble(context) : _otherBubble(context),
+        ],
+      ),
     );
   }
 
   Widget _myBubble(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: AppDimens.size4),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppText("나",
-                  style: Theme.of(context).textTheme.textSM.copyWith(
-                      color: AppColor.gray700,
-                      fontWeight: AppFontWeight.medium)),
-              AppText(format(message.createdAt),
-                  style: Theme.of(context).textTheme.textXS.copyWith(
-                      color: AppColor.gray600,
-                      fontWeight: AppFontWeight.regular))
-            ],
-          ),
-        ),
-        AppSpacerV(value: AppDimens.size4),
-        Container(
-          width: double.infinity, // 수정된 부분: Container의 width를 확장
-          padding: EdgeInsets.symmetric(
-            vertical: AppDimens.size8,
-            horizontal: AppDimens.size12,
-          ),
-          decoration: BoxDecoration(
-            color: AppColor.brand600,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(AppDimens.size8),
-              bottomLeft: Radius.circular(AppDimens.size8),
-              bottomRight: Radius.circular(AppDimens.size8),
-            ),
-            border: Border.all(
-              color: AppColor.brand600,
-              width: 1.0,
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.09,
+              child: const SizedBox(
+                  // width: AppDimens.size40,
+                  // height: AppDimens.size40,
+                  ),
             ),
           ),
-          child: AppText(
-            message.content,
-            maxLines: null,
-            style: Theme.of(context).textTheme.textMD.copyWith(
-                  color: AppColor.white,
-                  fontWeight: AppFontWeight.regular,
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: AppDimens.size4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AppText(
+                        "나",
+                        style: Theme.of(context).textTheme.textSM.copyWith(
+                              color: AppColor.gray700,
+                              fontWeight: AppFontWeight.medium,
+                            ),
+                      ),
+                      AppSpacerH(value: AppDimens.size4),
+                      AppText(
+                        format(message.createdAt),
+                        style: Theme.of(context).textTheme.textXS.copyWith(
+                              color: AppColor.gray600,
+                              fontWeight: AppFontWeight.regular,
+                            ),
+                      ),
+                    ],
+                  ),
                 ),
+                AppSpacerV(value: AppDimens.size4),
+                Container(
+                  width: double.infinity, // 내용 영역을 가능한 한 최대로 확장
+                  padding: EdgeInsets.symmetric(
+                    vertical: AppDimens.size8,
+                    horizontal: AppDimens.size12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColor.brand600,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(AppDimens.size8),
+                      bottomLeft: Radius.circular(AppDimens.size8),
+                      bottomRight: Radius.circular(AppDimens.size8),
+                    ),
+                    border: Border.all(
+                      color: AppColor.brand600,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: AppText(
+                    message.content,
+                    maxLines: null,
+                    style: Theme.of(context).textTheme.textMD.copyWith(
+                          color: AppColor.white,
+                          fontWeight: AppFontWeight.regular,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
   Widget _otherBubble(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const AppAvatar(),
-        AppSpacerH(value: AppDimens.size10),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: AppDimens.size4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    AppText('안녕',
-                        style: Theme.of(context).textTheme.textSM.copyWith(
-                            color: AppColor.gray700,
-                            fontWeight: AppFontWeight.medium)),
-                    AppText(format(message.createdAt),
-                        style: Theme.of(context).textTheme.textXS.copyWith(
-                            color: AppColor.gray600,
-                            fontWeight: AppFontWeight.regular))
-                  ],
-                ),
-              ),
-              AppSpacerV(value: AppDimens.size4), // 이름과 시간 간격 조정
-              Container(
-                width: double.infinity,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                decoration: BoxDecoration(
-                  color: AppColor.gray200,
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(8),
-                    bottomLeft: Radius.circular(8),
-                    bottomRight: Radius.circular(8),
-                  ),
-                  border: Border.all(
-                    color: AppColor.gray200,
-                    width: 1.0,
-                  ),
-                ),
-                child: AppText(
-                  message.content,
-                  maxLines: null,
-                  style: Theme.of(context).textTheme.textMD.copyWith(
-                        color: AppColor.gray900,
-                        fontWeight: AppFontWeight.regular,
-                      ),
-                ),
-              ),
-            ],
+    return Expanded(
+      child: Row(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.1,
+            child: const AppAvatar(),
           ),
-        ),
-      ],
+          AppSpacerH(value: AppDimens.size10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: AppDimens.size4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AppText(
+                        '안녕',
+                        style: Theme.of(context).textTheme.textSM.copyWith(
+                              color: AppColor.gray700,
+                              fontWeight: AppFontWeight.medium,
+                            ),
+                      ),
+                      AppText(
+                        format(message.createdAt),
+                        style: Theme.of(context).textTheme.textXS.copyWith(
+                              color: AppColor.gray600,
+                              fontWeight: AppFontWeight.regular,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+                AppSpacerV(value: AppDimens.size4),
+                Container(
+                  width: double.infinity, // 내용 영역을 가능한 한 최대로 확장
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColor.gray200,
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(8),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8),
+                    ),
+                    border: Border.all(
+                      color: AppColor.gray200,
+                      width: 1.0,
+                    ),
+                  ),
+                  child: AppText(
+                    message.content,
+                    maxLines: null,
+                    style: Theme.of(context).textTheme.textMD.copyWith(
+                          color: AppColor.gray900,
+                          fontWeight: AppFontWeight.regular,
+                        ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
